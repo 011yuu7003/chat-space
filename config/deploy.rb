f203c29c9b9cfa2452f211ec42103a73e5827493
011yuu7003 <56720641+011yuu7003@users.noreply.github.com>
2019-11-01 13:28:04 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,18 @@
-# config valid for current version and patch releases of Capistrano
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.11.2'
 
-set :application, "chat-space"
-set :repo_url, "git@example.com:011yuu7003/chat-space"
+# Capistranoのログの表示に利用する
+set :application, 'chat-space'
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@example.com:011yuu7003/chat-space.git'
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
+# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
