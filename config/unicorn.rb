f1f1b930af5fe963a2d8e498f3b8db2af369b16f
011yuu7003 <56720641+011yuu7003@users.noreply.github.com>
2019-11-01 15:40:46 +0900

    a

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ working_directory "#{app_path}/current"
 
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+listen 3000
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
